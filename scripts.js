96c48b1cb02b50cb75c462bb56163159af338652
Nika Darab <nika.darab@gmail.com>
2020-07-31 10:51:33 -0500

    moved the script tag to the bottom

--- scripts.js
+++ scripts.js
@@ -6,19 +6,19 @@ function setDate (){
     const now = new Date();
 
     const seconds = now.getSeconds();
-    const secondsDegree = (seconds * 6) + 90;
+    const secondsDegree = ((seconds /60)* 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 
     const minutes = now.getMinutes();
-    const minutesDegree = (minutes * 6) + (seconds/10) + 90;
-    minsHand.style.transform = `rotate(${minutesDegree}deg)`;
+    const minutesDegree = ((minutes /60) * 360) + ((seconds/60)*6) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
 
     const hours = now.getHours();
-    const hoursDegree = (hours * 30) + (minutes/2) + 90;
+    const hoursDegree = ((hours /12)*360) + ((minutes/60)*30) + 90;
     hourHand.style.transform = `rotate(${hoursDegree}deg)`;
 }
 
-setInterval(setDate, 1000);
+setInterval(setDate,1000);
 
 setDate();
 
