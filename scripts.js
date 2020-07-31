556c03be6bdca783e5ad6158d2185226bd0ceace
Nika Darab <nika.darab@gmail.com>
2020-07-30 20:21:46 -0500

    setDate function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-const secondHand = document.querySelector(.second-hand)
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate (){
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegree = (seconds * 6) + 90;
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegree = (minutes * 6) + (seconds/10) + 90;
+    minsHand.style.transform = `rotate(${minutesDegree}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegree = (hours * 30) + (minutes/2) + 90;
+    hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate();
+
